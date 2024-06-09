0e24fddf7b6d726e7ebf762c10404132bb99b374
Neelash Kannan <115584382+neelashkannan@users.noreply.github.com>
2024-06-09 20:58:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -21,12 +21,12 @@ with col1:
 with col2:
     a = st.selectbox("Choose the model", options=["Baby Cobolt","Teenage Cobolt"])
     
- 
+ mname = ""
 if a == "Baby Cobolt":
-   global manme = "demo1"
+    manme = "demo1"
 elif a == "Teenage Cobolt":
     m = "llama3"
-    global manme = "demo2"
+    manme = "demo2"
 
 
 
@@ -40,25 +40,27 @@ for message in st.session_state.messages:
         st.markdown(' '.join(message["content"].split('\n')), unsafe_allow_html=True)
 
 # React to user input
-if prompt := st.chat_input("What is up?"):
+if mname != None:
+    
+    if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(' '.join(prompt.split('\n')), unsafe_allow_html=True)
-        stream = ollama.chat(
+        with st.chat_message("user"):
+            st.markdown(' '.join(prompt.split('\n')), unsafe_allow_html=True)
+            stream = ollama.chat(
             model=mname,
             messages=[{'role': 'user', 'content': ' '.join(prompt.split('\n'))}],
             stream=True,
-        )
+            )
     # Add user message to chat history
-    st.session_state.messages.append({"role": "robot", "content": ' '.join(prompt.split('\n'))})
+        st.session_state.messages.append({"role": "robot", "content": ' '.join(prompt.split('\n'))})
 
-    response = f"Echo: {prompt}"
+        response = f"Echo: {prompt}"
     # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        response_text = st.empty()  # placeholder for the text
-        full_response = ""
-        for chunk in stream:
-            full_response += chunk['message']['content']  # concatenate each chunk to the existing text
-            response_text.markdown(f"<p style='word-wrap: break-word;'>{full_response}</p>", unsafe_allow_html=True)
+        with st.chat_message("assistant"):
+            response_text = st.empty()  # placeholder for the text
+            full_response = ""
+            for chunk in stream:
+                full_response += chunk['message']['content']  # concatenate each chunk to the existing text
+                response_text.markdown(f"<p style='word-wrap: break-word;'>{full_response}</p>", unsafe_allow_html=True)
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": f"<p style='word-wrap: break-word;'>{full_response}</p>"})
+        st.session_state.messages.append({"role": "assistant", "content": f"<p style='word-wrap: break-word;'>{full_response}</p>"})
