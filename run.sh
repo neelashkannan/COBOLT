f22237c93768c3c965026f570cfd8719cc83794a
Neelash Kannan <115584382+neelashkannan@users.noreply.github.com>
2024-07-24 20:36:30 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,4 @@
 ollama serve &
-sleep 5
-
-ollama create demo1 -f Modelfile &
-
-sleep 10
-
-ollama create demo2 -f Modelfile2 &
-
-sleep 10
-
-ollama create demo3 -f Modelfile3 &
-
-sleep 15
-pip3 install open-webui &
 
 sleep 10
 
